73c789594ef98f0d9219ede65682f02dbde6cb5e
anshulsoni4 <anshulsoni3040@gmail.com>
2024-05-04 12:33:47 +0530

    edits

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -32,7 +32,7 @@ const ImageGallery = () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center items-center m-auto p-1">
+      <div className="flex justify-center items-center m-4 p-2">
       <button
         className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         onClick={handleLoadMore}
